6b6a19658e1cf4a318519043c9d1006b8a3829cd
Milyra35 <marie.richir35@gmail.com>
2023-06-26 12:56:43 +0000

    suite exercice Yepages avec Vue

--- assets/js/components/ListOfBooks.js
+++ assets/js/components/ListOfBooks.js
@@ -1,12 +1,29 @@
-let ListOfBooks = {
-    props : [
-        "books"
-    ],
-    template: `
-        <section>
-            
-        </section>
-    `
-};
+let bookList = "https://res04.sites.3wa.io/yepages-api/books.json";
+let ListOfBooks;
 
-export { ListOfBooks } from './ListOfBooks.js';
+fetch(bookList)
+.then(function(response) {
+    return response.json();
+})
+.then(function(result) {
+    console.log(result);
+    
+    ListOfBooks = {
+        props : [
+            "books"
+        ],
+        template: `
+            <article v-for="book in books">
+                <img src={{ book.image }}>
+                <h4>{{ book.title }}</h4>
+                <p class="category">{{ book.category }}</p>
+                <p class="author">{{ book.author }}</p>
+                <button>Découvrir le livre</button>
+            </article>
+        `
+    };
+})
+
+
+
+export { ListOfBooks };
--- assets/js/components/Main.js
+++ assets/js/components/Main.js
@@ -1,20 +1,23 @@
-
+import { ListOfBooks } from './ListOfBooks.js';
 
 let Main = {
     data() {
         return {
-            
-        }
+            books : [
+            ]
+        };
     },
     components : {
-        ListOfBooks;
+        ListOfBooks
     },
     template : `
-    <main>
-        <h3>Découvrez <span>notre collection d'ouvrages</span></h3>
-        <ListOfBooks />
-    </main>
-    `
+        <main>
+            <h3>Découvrez <span>notre collection d'ouvrages</span></h3>
+            <section class="list-of-books" id="list-of-books">
+                <ListOfBooks :books="books"/>
+            </section>
+        </main>
+    `,
 };
 
 export { Main };
